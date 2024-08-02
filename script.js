b1f87f8132b9d1158f4bb4286083846ca7b2cc68
avinvadas <avin@avinvadas.com>
2024-08-02 13:56:15 +0100

    auto-focus on type, clear secondary label jump

--- script.js
+++ script.js
@@ -150,12 +150,14 @@ function copyPaletteToClipboard(palette) {
 //INITIATING
 function init() {
   document.documentElement.style.setProperty('--color_primary-hex', getRandomHexColor())
+  
   presentColorValues('colorTicker');
   setInput();
   setColorPicker();
   setSliders();
   alignInputsToHex();
   checkInputContrast();
+  setInputAutoFocus("colorInput");
 }
 
 /*Color manipulation inputs*/
@@ -403,5 +405,30 @@ function hexToHSL(hex, component) {
   }
 }
 
+function setInputAutoFocus(input){
+  // Select the input field you want to focus
+  const inputField = document.getElementById(input);
+
+  // Variable to track if focus has been set
+  let focusSet = false;
+
+  // Add an event listener to the document for keydown events
+  document.addEventListener('keydown', function() {
+      if (!focusSet) {
+          inputField.focus();
+          focusSet = true; // Set the flag to prevent repeated calls
+      }
+  });
+
+  // Add an event listener for focus on the input field to clear the value
+  inputField.addEventListener('focus', function() {
+      inputField.value = ''; // Clear the input field
+  });
+
+  // Reset the focusSet flag when the input field loses focus
+  inputField.addEventListener('blur', function() {
+      focusSet = false; // Resetting focusSet allows refocusing
+  });
+}
 /* assets---------------------------------------------------------------------*/
 const icon_copy = '<span class="icon_copy"><svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="currentColor" d="M384 336l-192 0c-8.8 0-16-7.2-16-16l0-256c0-8.8 7.2-16 16-16l140.1 0L400 115.9 400 320c0 8.8-7.2 16-16 16zM192 384l192 0c35.3 0 64-28.7 64-64l0-204.1c0-12.7-5.1-24.9-14.1-33.9L366.1 14.1c-9-9-21.2-14.1-33.9-14.1L192 0c-35.3 0-64 28.7-64 64l0 256c0 35.3 28.7 64 64 64zM64 128c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-32-48 0 0 32c0 8.8-7.2 16-16 16L64 464c-8.8 0-16-7.2-16-16l0-256c0-8.8 7.2-16 16-16l32 0 0-48-32 0z"></path></svg></span>';
